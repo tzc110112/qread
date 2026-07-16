1f28e7a4d4ab61f5168a82363cba2385dee12c02
唐 <tzc@tangdeMac-mini.local>
2026-07-16 16:57:45 +0800

    fix: ensure solon.config.add reads file system conf.yml
    使用 ./conf.yml 确保 Solon 读取文件系统的 /app/conf.yml，
    而非 classpath 内的 conf.yml。
    entrypoint 对环境变量的 sed 替换才能正确生效。

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -4,20 +4,38 @@
 CONF=/app/conf.yml
 JAR=/app/read.jar
 
-# admin
-[ -n "$ADMIN_USERNAME" ] && sed -i "s/^  username:.*/  username: \"$ADMIN_USERNAME\"/" $CONF
-[ -n "$ADMIN_PASSWORD" ] && sed -i "s/^  password:.*/  password: \"$ADMIN_PASSWORD\"/" $CONF
-[ -n "$ADMIN_GONGGAO" ] && sed -i "s/^  gonggao:.*/  gonggao: \"$ADMIN_GONGGAO\"/" $CONF
-[ -n "$ADMIN_UPDATE" ] && sed -i "s/^  update:.*/  update: $ADMIN_UPDATE/" $CONF
+# 用环境变量覆写 conf.yml
+if [ -n "$ADMIN_USERNAME" ]; then
+  sed -i "s/^  username: \".*\"/  username: \"$ADMIN_USERNAME\"/" $CONF
+fi
+if [ -n "$ADMIN_PASSWORD" ]; then
+  sed -i "s/^  password: \".*\"/  password: \"$ADMIN_PASSWORD\"/" $CONF
+fi
+if [ -n "$ADMIN_GONGGAO" ]; then
+  sed -i "s/^  gonggao: \".*\"/  gonggao: \"$ADMIN_GONGGAO\"/" $CONF
+fi
+if [ -n "$ADMIN_UPDATE" ]; then
+  sed -i "s/^  update: .*/  update: $ADMIN_UPDATE/" $CONF
+fi
+if [ -n "$USER_SOURCE" ]; then
+  sed -i "s/^  source: .*/  source: $USER_SOURCE/" $CONF
+fi
+if [ -n "$USER_ALLOWCHANGE" ]; then
+  sed -i "s/^  allowchange: .*/  allowchange: $USER_ALLOWCHANGE/" $CONF
+fi
+if [ -n "$USER_PROXYPNG" ]; then
+  sed -i "s/^  proxypng: .*/  proxypng: $USER_PROXYPNG/" $CONF
+fi
+if [ -n "$USER_INDEX" ]; then
+  sed -i "s/^  index: .*/  index: $USER_INDEX/" $CONF
+fi
+if [ -n "$AUTO_CRAWL" ]; then
+  sed -i "s/^  crawl: .*/  crawl: $AUTO_CRAWL/" $CONF
+fi
+if [ -n "$AUTO_UPDATE" ]; then
+  sed -i "s/^  update: true/  update: $AUTO_UPDATE/" $CONF
+fi
 
-# user
-[ -n "$USER_SOURCE" ] && sed -i "s/^  source: .*/  source: $USER_SOURCE/" $CONF
-[ -n "$USER_ALLOWCHANGE" ] && sed -i "s/^  allowchange: .*/  allowchange: $USER_ALLOWCHANGE/" $CONF
-[ -n "$USER_PROXYPNG" ] && sed -i "s/^  proxypng: .*/  proxypng: $USER_PROXYPNG/" $CONF
-[ -n "$USER_INDEX" ] && sed -i "s/^  index: .*/  index: $USER_INDEX/" $CONF
-
-# auto
-[ -n "$AUTO_CRAWL" ] && sed -i "s/^  crawl: .*/  crawl: $AUTO_CRAWL/" $CONF
-[ -n "$AUTO_UPDATE" ] && sed -i "s/^  update: true/  update: $AUTO_UPDATE/" $CONF
+# 环境变量未设置时，保持默认值不做修改
 
 exec java -jar $JAR
